bb045d64c5e997049787e75630ddc8e0952a23dc
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-05-15 21:56:11 +0100

    fix task_1 error

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 module.exports = {
-  entry: { bundle: path.resolve(__dirname, "js/dashboard_main.js") },
-  output: { path: path.resolve(__dirname, "public"), filename: "[name].js" },
+  mode: "production",
+  entry: path.resolve(__dirname, "js/dashboard_main.js"),
+  output: { path: path.resolve(__dirname, "public"), filename: "bundle.js" },
 };
