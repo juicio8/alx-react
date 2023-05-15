7467050f08160d72b7e7d3f6969e4cdabeec4574
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-05-15 20:29:22 +0100

    redefined directories

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+$(document).ready(() => {
+  $("body").append(
+    "<p>Holberton Dashboard</p><p>Dashboard data for the students</p><p>Copyright - Holberton School</p>"
+  );
+});
