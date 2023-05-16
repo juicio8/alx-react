ac51e6fa6fc8565c52f170924dd4b6e5f4309970
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-05-16 09:47:52 +0100

    fixing errors...

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
   mode: "production",
-  entry: { bundle: path.resolve(__dirname, "js/dashboard_main.js") },
-  output: { path: path.resolve(__dirname, "public"), filename: "[name].js" },
+  entry: path.resolve(__dirname, "js/dashboard_main.js"),
+  output: { path: path.resolve(__dirname, "public"), filename: "bundle.js" },
 };
